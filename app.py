99df8ddf3a68716aae28bafaea6fc20e66c54d87
dev02chandan <dev02chandan@gmail.com>
2024-05-14 14:32:44 +0530

    Conditional Statement, added functionality of new item

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ processor = BlipProcessor.from_pretrained(model_name)
 model = BlipForConditionalGeneration.from_pretrained(model_name)
 
 # Function to generate captions
-def generate_caption(image, include_object, object_choice=None):
-    if include_object and object_choice:
-        caption_prefix = f"The {object_choice} in the image is"
+def generate_caption(image, object_choice=None):
+    if object_choice and object_choice != "None":
+        caption_prefix = f"The condition of the {object_choice} in the image is"
         inputs = processor(image, caption_prefix, return_tensors="pt")
     else:
         inputs = processor(image, return_tensors="pt")
@@ -19,8 +19,9 @@ def generate_caption(image, include_object, object_choice=None):
     return caption
 
 # Streamlit interface
-st.title("Image Captioning Application")
-st.write("Upload an image and optionally select an object to include in the caption.")
+st.image("logo.png", use_column_width=False)
+st.title("Image-Based Object Condition Assessment")
+st.write("Upload an image and optionally select or type an object to include in the caption.")
 
 # File uploader
 uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
@@ -28,20 +29,17 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
     st.image(image, caption='Uploaded Image', use_column_width=True)
 
-    # Checkbox to choose whether to include an object in the caption
-    include_object = st.checkbox("Include an object in the caption?")
+    # Dropdown for selecting the object and allowing custom input
+    object_choice = st.selectbox(
+        "Choose or type an object:",
+        ("None", "carpet", "sofa", "table", "chair", "lamp", "glass", "wall", "bed", "other"),
+        index=0  # Default to "None"
+    )
 
-    object_choice = None
-    if include_object:
-        # Dropdown for selecting the object if the user chooses to include it
-        object_choice = st.selectbox(
-            "Choose an object:",
-            ("carpet", "sofa", "table", "chair", "lamp"),
-            index=0  # Default to the first item
-        )
+    if object_choice == "other":
+        object_choice = st.text_input("Type your object:")
 
     # Button to generate caption
     if st.button("Generate Caption"):
-        caption = generate_caption(image, include_object, object_choice)
+        caption = generate_caption(image, object_choice)
         st.write(caption)
-
